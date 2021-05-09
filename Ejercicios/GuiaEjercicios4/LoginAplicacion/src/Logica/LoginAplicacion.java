631ec5acd4261120ad43482d4bea8ec3964815b1
Trinidad Boragini <54488386+TriniBora@users.noreply.github.com>
2021-05-09 14:18:30 -0300

    Add files via upload

---
+++ Ejercicios/GuiaEjercicios4/LoginAplicacion/src/Logica/LoginAplicacion.java
@@ -0,0 +1,17 @@
+package Logica;
+
+import IGU.Login;
+
+public class LoginAplicacion {
+
+    public static void main(String[] args) {
+        
+        Login formulario = new Login();
+        formulario.setVisible(true);
+        formulario.setLocationRelativeTo(null);
+                
+                
+        
+    }
+    
+}
